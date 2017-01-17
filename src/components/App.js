68db8e9059af56359f2fbd9361f90836501ba79f
Nicky Semenza <nicky@nickysemenza.com>
2017-01-16 23:32:19 -0500

    fix navbar hiding content

--- src/components/App.js
+++ src/components/App.js
@@ -4,6 +4,14 @@ export default class App extends Component {
   static propTypes = {
     children: PropTypes.element.isRequired
   };
+  componentWillMount () {
+    if (this.props.isAuthenticated) {
+      document.getElementById('body').className = 'bodyPad';
+    }
+  }
+  componentWillUnmount () {
+    document.getElementById('body').className = '';
+  }
   render () {
     return (
       <div className='main-app'>
