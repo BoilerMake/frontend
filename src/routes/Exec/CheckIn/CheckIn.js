fef8fba05c8f492ac39eeab5ed474896b395c93d
Nicky Semenza <nicky@nickysemenza.com>
2017-01-20 12:09:31 -0500

    allow override on checkin

--- src/routes/Exec/CheckIn/CheckIn.js
+++ src/routes/Exec/CheckIn/CheckIn.js
@@ -3,14 +3,19 @@ import CheckInList from './CheckInList';
 export default class CheckIn extends Component {
   constructor (props) {
     super(props);
-    this.state = { searchValue: '' };
+    this.state = { searchValue: '', isChecked: false };
     this.handleChange = this.handleChange.bind(this);
+    this.onCheckboxChange = this.onCheckboxChange.bind(this);
   }
   componentDidMount () {
     this.props.loadData();
   }
   handleChange (event) {
     this.setState({ searchValue: event.target.value });
+  }
+
+  onCheckboxChange () {
+    this.setState({ isChecked: !this.state.isChecked });
   }
 
   render () {
@@ -23,6 +28,7 @@ export default class CheckIn extends Component {
         return elem.first_name.toLowerCase().includes(searchVal)
           || elem.last_name.toLowerCase().includes(searchVal)
           || elem.launch.toLowerCase().includes(searchVal)
+          || elem.email.toLowerCase().includes(searchVal)
           || String(elem.id).includes(searchVal)
           || (elem.application.school ? elem.application.school.name.toLowerCase().includes(searchVal) : false);
       }
@@ -38,13 +44,23 @@ export default class CheckIn extends Component {
             <input type='text' placeholder='type name, school, ID' value={this.state.searchValue} onChange={this.handleChange} />
             <br />
             <small>showing {filteredHackers.length <= maxLen ? filteredHackers.length : maxLen} out of {filteredHackers.length}</small>
+
+            <br />
+            Override disabled checkin
+            <input
+              type='checkbox'
+              checked={this.state.isChecked}
+              onChange={this.onCheckboxChange}
+            />
+
           </div>
           <div className='col-md-6'>
             <button className='button-primary-wide' onClick={this.props.loadData}>Reload Data</button>
           </div>
 
         </div>
-        <CheckInList hackers={shortenedHackers} checkIn={this.props.checkIn} />
+        {this.state.isChecked ? <h1 style={{ color: 'orange' }}>!! Check In Override Enabled !!</h1> : ''}
+        <CheckInList hackers={shortenedHackers} override={this.state.isChecked} checkIn={this.props.checkIn} />
       </div>
     </div>
     );
--- src/routes/Exec/CheckIn/CheckInList.js
+++ src/routes/Exec/CheckIn/CheckInList.js
@@ -8,10 +8,14 @@ export default class CheckInList extends Component {
     let hackers = this.props.hackers;
     let hackerList = hackers.map(hacker => {
       let cleared = hacker.application.rsvp && (hacker.application.checked_in_at === null);
+      cleared = this.props.override ? true : cleared;
+
       return <div className='panel panel-default' key={hacker.id}>
         <div className='panel-heading'>{hacker.first_name} {hacker.last_name} (#{hacker.id}) <Link to={`/exec/users/${hacker.id}`}>view</Link></div>
         <div className='panel-body'>
           <strong>School:</strong> {hacker.application.school ? hacker.application.school.name : ''}
+          <br />
+          <strong>Email:</strong> {hacker.email}
           <br />
           <strong>RSVP:</strong> {hacker.application.rsvp ? 'yes' : 'no'}
           <br />
@@ -25,7 +29,7 @@ export default class CheckInList extends Component {
           <br />
           <strong>Checked in:</strong> {hacker.application.checked_in_at ? moment(hacker.application.checked_in_at).fromNow() : 'not yet'}
           <br />
-          <button onClick={() => this.props.checkIn(hacker.id)} disabled={!cleared}>Check In</button>
+          <button onClick={() => this.props.checkIn(hacker.id)} className='checkInButton' disabled={!cleared}>Check In</button>
         </div>
       </div>;
     });
