f30a3c5597d4a0553501f2658c411b621b30a827
Nicky Semenza <nicky@nickysemenza.com>
2017-04-18 22:27:44 -0400

    about page test

---
+++ src/pages/About.test.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import About from './About';
+import { MemoryRouter } from 'react-router-dom';
+
+it('renders without crashing', () => {
+    const div = document.createElement('div');
+    ReactDOM.render(<MemoryRouter>
+            <About />
+        </MemoryRouter>
+        , div);
+});
+
