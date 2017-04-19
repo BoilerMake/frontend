dd163807f8c612ae7b4885857716675849ba6c4a
Nicky Semenza <nicky@nickysemenza.com>
2017-04-19 00:52:25 -0400

    persist JWT to cookie

--- src/actions/users.js
+++ src/actions/users.js
@@ -1,5 +1,7 @@
 export const LOGIN_FROM_JWT_SUCCESS = 'LOGIN_FROM_JWT_SUCCESS';
+import cookie from 'react-cookie';
 export function loginFromJWT (token) {
+	cookie.save('token',token, {path: '/'});
     return {
         type: LOGIN_FROM_JWT_SUCCESS,
         token: token
--- src/index.js
+++ src/index.js
@@ -3,11 +3,14 @@ import ReactDOM from 'react-dom';
 import App from './App';
 import './assets/index.scss';
 import 'bootstrap/dist/css/bootstrap.css';
-
-
+import cookie from 'react-cookie';
 import { Provider } from 'react-redux'
 import configureStore from './store/configureStore'
+
 const store = configureStore();
+
+const token = cookie.load('token');
+if (token) store.dispatch({ type: 'LOGIN_FROM_JWT_SUCCESS' });//log us in from cookie
 
 ReactDOM.render(
     <Provider store={store}>
