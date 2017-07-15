9bdc239287ec5029fd09b0a7b5826bbed261e364
Nicky Semenza <nicky@nickysemenza.com>
2017-07-14 21:23:32 -0700

    add GITHUB_CLIENT_ID to config exmaple

--- src/config.example.js
+++ src/config.example.js
@@ -2,3 +2,4 @@ export const API_BASE_URL = 'http://localhost:8000/v1';// no trailing slashes!
 export const GOOGLE_ANALYTICS_ID = 'UA-00000000-1';
 export const DEBUG_MODE = true;
 export const SENTRY_URL = '';
+export const GITHUB_CLIENT_ID = '';
