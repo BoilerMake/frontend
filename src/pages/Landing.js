84914a1fb1a990d564aad124491f7e2383c3ae21
Cole Johnson <colehjohn@gmail.com>
2017-07-27 20:19:58 -0400

    Homepage fi

--- src/pages/Landing.js
+++ src/pages/Landing.js
@@ -4,15 +4,17 @@ import B from '../assets/images/b.png';
 
 const Landing = () => (
     <div className="page bgdarkBlue">
-        <div className="con flexCon">
-            <div className="flexchild">
-                <h1 className="white title">BoilerMake</h1>
-                <h3 className="pink title">Purdue University</h3>
-                <h3 className="lightBlue title">SEPT 29  - OCT 1 2017</h3>
-                <Interest/>
-            </div>
-            <div className="flexchild">
-                <img src={B} alt="boilermakev-logo" />
+        <div className="max-width">
+            <div className="row">
+                <div className="col-6">
+                    <h1 className="white title">BoilerMake</h1>
+                    <h3 className="pink title">Purdue University</h3>
+                    <h3 className="lightBlue title">SEPT 29  - OCT 1 2017</h3>
+                    <Interest/>
+                </div>
+                <div className="col-6">
+                    <img src={B} alt="boilermakev-logo" style={ { 'max-width': '400px' } } />
+                </div>
             </div>
         </div>
     </div>
