aaea1ff5a7946a779d361d4890204a70de3e21f4
Nicky Semenza <nicky@nickysemenza.com>
2017-07-30 01:09:34 -0700

    switch to using env vars for config

--- src/config.example.js
+++
@@ -1,6 +0,0 @@
-export const API_BASE_URL = 'http://localhost:8000/v1';// no trailing slashes!
-export const GOOGLE_ANALYTICS_ID = 'UA-00000000-1';
-export const DEBUG_MODE = true;
-export const SENTRY_URL = '';
-export const GITHUB_CLIENT_ID = '';
-export const ALLOW_SIGNUPS = false;
