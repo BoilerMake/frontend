849f2696ab649fb88d81811169707cf23e27387f
Cole Johnson <colehjohn@gmail.com>
2017-07-31 18:16:55 -0400

    Remove hawaii theme

--- src/pages/Application/ApplicationDropdown.js
+++ src/pages/Application/ApplicationDropdown.js
@@ -13,10 +13,6 @@ const ApplicationDropdown = ({changeApplicationFieldValue, field, applicationFor
       className="dropdown"
     >
         { Object.keys(options).map((t,i) => <option key={i} value={t}>{t}</option>) }
-        {/* <option value="grapefruit">Grapefruit</option>
-        <option value="lime">Lime</option>
-        <option value="coconut">Coconut</option>
-        <option value="mango">Mango</option> */}
     </select>
 );
 
