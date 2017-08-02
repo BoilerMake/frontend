a67ffab6a1e46fe46657407fb64533472017dd08
Cole Johnson <colehjohn@gmail.com>
2017-08-02 18:37:35 -0400

    Cleaner register and login

--- src/pages/Register/RegisterForm.js
+++ src/pages/Register/RegisterForm.js
@@ -1,11 +1,11 @@
 import React from 'react'
 import { Field, reduxForm } from 'redux-form'
 import GithubLoginButton from '../../components/GithubLoginButton'
+import { Link } from 'react-router-dom'
 import '../../assets/_form.scss'
 
 const renderField = ({ input, label, type, meta: { touched, error } }) => (
     <div>
-        <label>{label}</label>
         <div>
             <input {...input} placeholder={label} type={type}/>
             {touched && error && <span>{error}</span>}
@@ -17,14 +17,16 @@ export const RegisterForm = (props) => {
     const { error, handleSubmit, submitting } = props;
     return (
         <form onSubmit={handleSubmit} className="form">
-          <h1>Register</h1>
+          <h1 className="title text-center">Register</h1>
+          <GithubLoginButton className="topSpacing" />
+          <div className="white">Register with Email</div>
           <Field name="email" type="email" component={renderField} label="Email"/>
-          <br/>
           <Field name="password" type="password" component={renderField} label="Password"/>
-          {error && <strong>{error}</strong>}
+          {error && <div className="margint">{error}</div>}
           <button className="btn topSpacing" type="submit" disabled={submitting}>Register</button>
-          <br/><br/>
-          <GithubLoginButton className="topSpacing"  />
+          <div className="flex margint h-center">
+            <Link to="/login">Back to Login</Link>
+          </div>
         </form>
     )
 }
