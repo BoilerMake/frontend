a20b3a9a60ee52264cf4c02f7a42f7ad9d7a229d
Nicky Semenza <nicky@nickysemenza.com>
2017-08-02 15:51:26 -0700

    oops

--- src/pages/Register/RegisterForm.test.js
+++ src/pages/Register/RegisterForm.test.js
@@ -6,8 +6,6 @@ import { shallow, mount } from "enzyme";
 
 import { createStore } from 'redux';
 import { Provider } from 'react-redux';
-import { MemoryRouter } from 'react-router-dom';
-
 
 it('renders without crashing', () => {
     const div = document.createElement('div');
