2cf8db06a3a79cce391576258f01b2cf6cae5928
Nicky Semenza <nicky@nickysemenza.com>
2017-08-02 15:47:54 -0700

    fix registerform test?

--- src/pages/Register/RegisterForm.test.js
+++ src/pages/Register/RegisterForm.test.js
@@ -6,15 +6,18 @@ import { shallow, mount } from "enzyme";
 
 import { createStore } from 'redux';
 import { Provider } from 'react-redux';
+import { MemoryRouter } from 'react-router-dom';
 
 
 it('renders without crashing', () => {
     const div = document.createElement('div');
     const store = createStore(() => ({}));
     ReactDOM.render(
-        <Provider store={store}>
-            <RegisterForm/>
-        </Provider>
+        <MemoryRouter>
+            <Provider store={store}>
+                <RegisterForm/>
+            </Provider>
+        </MemoryRouter>
         , div);
 });
 
