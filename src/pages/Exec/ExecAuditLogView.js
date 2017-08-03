f8a9afefb43b15a04b1dddfbdd12f360760d2dcd
Nicky Semenza <nicky@nickysemenza.com>
2017-08-02 18:22:06 -0700

    display Application and User audit logs

--- src/pages/Exec/ExecAuditLogView.js
+++ src/pages/Exec/ExecAuditLogView.js
@@ -1,7 +1,17 @@
 import React from 'react';
 import moment from 'moment';
 const ExecAuditLogView = ({audits}) => {
-    return(<pre>{JSON.stringify(audits,null,2)}</pre>);
+    // return(<pre>{JSON.stringify(audits,null,2)}</pre>);
+    return(<div>
+        {audits.map(audit=> {
+            let when = moment(audit.created_at);
+            let { old_values, new_values } = audit;
+            return(<div key={audit.id}>
+                {when.fromNow()} <i>({when.format()})</i>
+                <pre>{JSON.stringify({ old_values, new_values },null,2)}</pre>
+            </div>)
+        })}
+    </div>)
 };
 ExecAuditLogView.displayName = 'ExecAuditLogView';
 export default ExecAuditLogView;
