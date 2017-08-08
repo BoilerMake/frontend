27d9890fe6cc1c4116e264f1ecaf13bc714fd104
avincent6 <vincent6@purdue.edu>
2017-08-07 23:56:06 -0600

    make sign animation infinite and slower, make pillars less subtle, and add register button

--- src/components/Animation.js
+++ src/components/Animation.js
@@ -6,6 +6,10 @@ import '../assets/pillars.scss';
 const Animation = () => (
   <div className='animation'>
   <img className="sign" src={sign} alt="logo-sign" />
+  <div className="register">
+    <h3>Purdue University • September 29 - October 1, 2017</h3>
+    <button className="btn">register</button>
+  </div>
   <img className="pillars" src={pillars} alt="pillars" />
   </div>
 );
