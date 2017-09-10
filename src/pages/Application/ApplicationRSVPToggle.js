dd1f81d3076ff6a784c823e194b037122b95d515
Nicky Semenza <nicky@nickysemenza.com>
2017-09-09 23:32:21 -0400

    rsvp v2

--- src/pages/Application/ApplicationRSVPToggle.js
+++ src/pages/Application/ApplicationRSVPToggle.js
@@ -5,8 +5,18 @@ import { connect } from 'react-redux';
 
 const ApplicationRSVPToggle = ({changeApplicationFieldValue, saveApplication, field, applicationForm, disabled, isLoading, styles}) =>
         <div>
-            <button disabled={applicationForm.rsvp === 1 || isLoading} className={applicationForm.rsvp !== null && applicationForm.rsvp === 1 ? 'rsvp-button-selected' : 'rsvp-button'} onClick={()=>{changeApplicationFieldValue('rsvp', 1); saveApplication(true)}}>YAY</button>
-            <button disabled={applicationForm.rsvp === 0 || isLoading} className={applicationForm.rsvp !== null && applicationForm.rsvp === 0 ? 'rsvp-button-selected' : 'rsvp-button'} onClick={()=>{changeApplicationFieldValue('rsvp', 0); saveApplication(true)}}>noo</button>
+            <button
+                disabled={applicationForm.rsvp === 1 || isLoading}
+                className={applicationForm.rsvp !== null && applicationForm.rsvp === 1 ? 'rsvp-button-selected' : 'rsvp-button'}
+                onClick={()=>{changeApplicationFieldValue('rsvp', 1); saveApplication(true)}}>
+                I'll be there!
+            </button>
+            <button
+                disabled={applicationForm.rsvp === 0 || isLoading}
+                className={applicationForm.rsvp !== null && applicationForm.rsvp === 0 ? 'rsvp-button-selected' : 'rsvp-button'}
+                onClick={()=>{changeApplicationFieldValue('rsvp', 0); saveApplication(true)}}>
+                I can't make it
+            </button>
         </div>;
 
 function mapStateToProps (state) {
