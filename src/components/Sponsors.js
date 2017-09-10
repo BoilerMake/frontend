e92fc5cbd6f1c9f88e26303c2186c4923f0ba0a6
avincent6 <vincent6@purdue.edu>
2017-09-10 17:25:47 -0400

    add sponsors to landing page

---
+++ src/components/Sponsors.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import '../assets/sponsors.scss';
+
+const Sponsors = () => (
+  <div className="s">
+    <div className="info">
+      <h1>BoilerMake V Partners</h1>
+      <p>Interested in partnering at BoilerMake?</p>
+      <p>Send us a message at <a href="mailto:sponsorship@boilermake.org">sponsorship@boilermake.org</a></p>
+    </div>
+    <div className="logo" />
+    <div className="logo" />
+    <div className="logo" />
+    <div className="logo" />
+    <div className="logo" />
+    <div className="logo" />
+    <div className="logo" />
+    <div className="logo" />
+    <div className="logo" />
+    <div className="info">
+      <h2>Special Thanks to</h2>
+    </div>
+    <div className="logo cs" />
+  </div>
+);
+
+export default Sponsors;
