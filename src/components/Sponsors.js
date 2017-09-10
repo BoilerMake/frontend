35a060001418d7307d8e64515128c936a4d9434e
avincent6 <vincent6@purdue.edu>
2017-09-10 17:42:07 -0400

    add sponsor links

--- src/components/Sponsors.js
+++ src/components/Sponsors.js
@@ -8,19 +8,20 @@ const Sponsors = () => (
       <p>Interested in partnering at BoilerMake?</p>
       <p>Send us a message at <a href="mailto:sponsorship@boilermake.org">sponsorship@boilermake.org</a></p>
     </div>
-    <div className="logo" />
-    <div className="logo" />
-    <div className="logo" />
-    <div className="logo" />
-    <div className="logo" />
-    <div className="logo" />
-    <div className="logo" />
-    <div className="logo" />
-    <div className="logo" />
+    <a href="https://www.capitalone.com" className="logo" />
+    <a href="https://www.ge.com/" className="logo" />
+    <a href="https://www.libertymutual.com" className="logo" />
+    <a href="https://mixmax.com/" className="logo" />
+    <a href="https://www.mongodb.com" className="logo" />
+    <a href="http://www.claritypartners.com/" className="logo" />
+    <a href="https://www.qualcomm.com" className="logo" />
+    <a href="http://www.rockwellcollins.com/" className="logo" />
+    <a href="http://techpoint.org/" className="logo" />
+    <a href="https://www.bloomberg.com/" className="logo" />
     <div className="info">
       <h2>Special Thanks to</h2>
     </div>
-    <div className="logo cs" />
+    <a href="https://www.cs.purdue.edu/" className="logo cs" />
   </div>
 );
 
