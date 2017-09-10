d6fb09a801d8a3e894834cab96df412b20b6b268
avincent6 <vincent6@purdue.edu>
2017-09-10 17:44:17 -0400

    add alt to images

--- src/components/Sponsors.js
+++ src/components/Sponsors.js
@@ -8,20 +8,20 @@ const Sponsors = () => (
       <p>Interested in partnering at BoilerMake?</p>
       <p>Send us a message at <a href="mailto:sponsorship@boilermake.org">sponsorship@boilermake.org</a></p>
     </div>
-    <a href="https://www.capitalone.com" className="logo" />
-    <a href="https://www.ge.com/" className="logo" />
-    <a href="https://www.libertymutual.com" className="logo" />
-    <a href="https://mixmax.com/" className="logo" />
-    <a href="https://www.mongodb.com" className="logo" />
-    <a href="http://www.claritypartners.com/" className="logo" />
-    <a href="https://www.qualcomm.com" className="logo" />
-    <a href="http://www.rockwellcollins.com/" className="logo" />
-    <a href="http://techpoint.org/" className="logo" />
-    <a href="https://www.bloomberg.com/" className="logo" />
+    <a href="https://www.capitalone.com" alt="capital one" className="logo" />
+    <a href="https://www.ge.com/" alt="general electric" className="logo" />
+    <a href="https://www.libertymutual.com" alt="liberty mutual" className="logo" />
+    <a href="https://mixmax.com/" alt="mixmax" className="logo" />
+    <a href="https://www.mongodb.com" alt="mongodb" className="logo" />
+    <a href="http://www.claritypartners.com/" alt="clarity partners" className="logo" />
+    <a href="https://www.qualcomm.com" alt="qualcomm" className="logo" />
+    <a href="http://www.rockwellcollins.com/" alt="rockwell collins" className="logo" />
+    <a href="http://techpoint.org/" alt="techpoint" className="logo" />
+    <a href="https://www.bloomberg.com/" alt="bloomberg" className="logo" />
     <div className="info">
       <h2>Special Thanks to</h2>
     </div>
-    <a href="https://www.cs.purdue.edu/" className="logo cs" />
+    <a href="https://www.cs.purdue.edu/" alt="purdue cs" className="logo cs" />
   </div>
 );
 
