0680b40558177de5eba25e406c64e082a758c1d3
Nicky Semenza <nicky@nickysemenza.com>
2017-09-11 18:43:13 -0400

    target blank

--- src/components/Sponsors.js
+++ src/components/Sponsors.js
@@ -7,16 +7,16 @@ const Sponsors = () => (
     <div className="info">
       <h1>Sponsors</h1>
     </div>
-    <a href="https://www.capitalone.com" alt="capital one" className="logo" />
-    <a href="https://www.ge.com/" alt="general electric" className="logo" />
-    <a href="https://www.libertymutual.com" alt="liberty mutual" className="logo" />
-    <a href="https://mixmax.com/" alt="mixmax" className="logo" />
-    <a href="https://www.mongodb.com" alt="mongodb" className="logo" />
-    <a href="http://www.claritypartners.com/" alt="clarity partners" className="logo" />
-    <a href="https://www.qualcomm.com" alt="qualcomm" className="logo" />
-    <a href="http://www.rockwellcollins.com/" alt="rockwell collins" className="logo" />
-    <a href="http://techpoint.org/" alt="techpoint" className="logo" />
-    <a href="https://www.bloomberg.com/" alt="bloomberg" className="logo" />
+    <a target="_blank" href="https://www.capitalone.com" alt="capital one" className="logo" />
+    <a target="_blank" href="https://www.ge.com/" alt="general electric" className="logo" />
+    <a target="_blank" href="https://www.libertymutual.com" alt="liberty mutual" className="logo" />
+    <a target="_blank" href="https://mixmax.com/" alt="mixmax" className="logo" />
+    <a target="_blank" href="https://www.mongodb.com" alt="mongodb" className="logo" />
+    <a target="_blank" href="http://www.claritypartners.com/" alt="clarity partners" className="logo" />
+    <a target="_blank" href="https://www.qualcomm.com" alt="qualcomm" className="logo" />
+    <a target="_blank" href="http://www.rockwellcollins.com/" alt="rockwell collins" className="logo" />
+    <a target="_blank" href="http://techpoint.org/" alt="techpoint" className="logo" />
+    <a target="_blank" href="https://www.bloomberg.com/" alt="bloomberg" className="logo" />
     <div className="info">
       <p>Interested in sponsoring BoilerMake?</p>
       <a href="mailto:sponsorship@boilermake.org"><div className="button">
