3a0d6ea1c90b3b2f7e9fac2fbdc6796e12e8aa8b
Nicky Semenza <nicky@nickysemenza.com>
2017-09-26 23:52:53 -0400

    moment utc test for prod

--- src/pages/Exec/ExecAuditLogView.js
+++ src/pages/Exec/ExecAuditLogView.js
@@ -4,7 +4,7 @@ const ExecAuditLogView = ({audits}) => {
     // return(<pre>{JSON.stringify(audits,null,2)}</pre>);
     return(<div>
         {audits.map(audit=> {
-            let when = moment(audit.created_at);
+            let when = moment.utc(audit.created_at);
             let { old_values, new_values } = audit;
             return(<div key={audit.id}>
                 {when.fromNow()} <i>({when.format()})</i>
