b38bf5ea96abae60d50ee471425a3b7a7d9fae81
Nicky Semenza <nicky@nickysemenza.com>
2017-09-28 01:54:45 -0400

    framework for live site

--- src/components/Schedule.js
+++ src/components/Schedule.js
@@ -2,22 +2,29 @@ import React from 'react';
 
 const friday = [{
     start: '6:00 PM',
-    name: 'Checkin begins'
+    name: 'Check in begins',
+    info: 'Loeb Playhouse'
 },{
     start: '8:00 PM',
-    name: 'Opening Ceremonies'
+    name: 'Opening Ceremonies',
+    info: 'Loeb Playhouse'
 },{
     start: '9:00 PM',
-    name: 'Team assembly'
+    name: 'Team assembly',
+    info: 'CoRec Black & Gold Gyms'
 },{
     start: '9:30 PM',
-    name: 'Dinner Served'
+    name: 'Dinner from Moe\'s',
+    info: 'taco bar (yes there\'s  queso)'
 },{
     start: '10:00 PM',
     name: 'Hacking Begins'
 },{
     start: '10:00 PM',
     name: 'Workshop/talk 1'
+},{
+    start: '11:59 PM',
+    name: 'Showers close'
 }];
 
 let saturday = [{
@@ -25,7 +32,11 @@ let saturday = [{
     name: 'Late Night Snack'
 },{
     start: '8:00 AM',
-    name: 'Breakfast'
+    name: 'Breakfast',
+    info: 'eggs, potatoes, bacon, fruit, bagels'
+},{
+    start: '8:00 AM',
+    name: 'Showers reopen'
 },{
     start: '9:00 AM',
     name: 'Workshop/talk 2'
@@ -37,7 +48,8 @@ let saturday = [{
     name: 'Yoga'
 },{
     start: '12:30 PM',
-    name: 'Lunch Served'
+    name: 'Lunch - Pasta Bar',
+    info: 'chicken, marinara, alfredo, veggies, salad, rolls'
 },{
     start: '1:00 PM',
     name: 'Workshop/talk 4'
@@ -55,10 +67,13 @@ let saturday = [{
     name: 'MLH Cup Stacking'
 },{
     start: '6:00 PM',
-    name: 'Dinner Served'
+    name: 'Dinner from Hotbox - pizza, breadsticks, salad'
 },{
     start: '6:00 PM',
     name: 'Workshop/talk 6'
+},{
+    start: '11:59 PM',
+    name: 'Showers close'
 }];
 
 let sunday = [{
@@ -66,7 +81,8 @@ let sunday = [{
     name: 'Late Night Snack'
 },{
     start: '8:00 AM',
-    name: 'Breakfast'
+    name: 'Breakfast',
+    info: 'egg casseroles, potatoes, fruit, yogurt, bagels, pastries'
 },{
     start: '9:30 AM',
     name: 'Hacking Ends'
@@ -74,23 +90,29 @@ let sunday = [{
     start: '10:00 AM',
     name: 'Expo Begins'
 },{
+    start: '10:00 AM',
+    name: 'Showers reopen'
+},{
     start: '11:30 AM',
-    name: 'Lunch Served'
+    name: 'Lunch from Potbelly',
+    info: 'sandwiches'
 },{
     start: '1:00 PM',
-    name: 'Closing Ceremonies'
+    name: 'Closing Ceremonies',
+    info: 'Loeb Playhouse'
 },{
     start: '2:30 PM',
-    name: 'Busses Depart'
+    name: 'Busses Depart',
+    info: 'Loeb Playhouse'
 }];
 
 const ScheduleDay =  ({dayData, title}) =>
     <div className="col-4 paddingx">
       <h3 className="center">{title}</h3>
         { dayData.map(item =>
-          <div className="schedule-item">
+          <div className="schedule-item" key={item.name+item.start}>
             <div className="time">{item.start}</div>
-            <div className="event">{item.name}</div>
+            <div className="event">{item.name} <br/><i>{item.info}</i></div>
           </div>
         )}
     </div>;
--- src/pages/DayOf/index.js
+++ src/pages/DayOf/index.js
@@ -1,7 +1,22 @@
 import React from 'react';
+import Schedule from '../../components/Schedule';
+import FAQ from '../Info/FAQ';
+import Announcements from './Announcements';
+
 const DayOf = () =>
-    <div className="fullWidthContainer application" style={{height: '1000px'}}>
-        <h1 className="title app-heading left">Day Of site coming soon... </h1>
+    <div className="landing-page">
+        <div className="announcements">
+            <h1 className="center title">Quick Links</h1>
+            <div className="row">
+                <ul>
+                    <li>Need technical help with your project? <a href="https://help.boilermake.org/">help.boilermake.org</a></li>
+                    <li>Want to socialize or ask questions, etc?<a href="http://invite.boilermake.org">hop on slack!</a></li>
+                </ul>
+            </div>
+        </div>
+        <Announcements/>
+        <Schedule/>
+        <FAQ/>
     </div>
 
 export default DayOf
