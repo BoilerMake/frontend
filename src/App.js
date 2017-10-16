e869adc415d469ad4c784df03ca0fa877b4b4fb6
Cole Johnson <colehjohn@gmail.com>
2017-10-15 20:07:12 -0400

    Removed footer def

--- src/App.js
+++ src/App.js
@@ -2,7 +2,6 @@ import React from 'react';
 import { BrowserRouter } from 'react-router-dom';
 import AnalyticsListener from './components/AnalyticsListener';
 import Routes from './Routes';
-import Footer from './components/Footer';
 import ReduxToastr from 'react-redux-toastr'
 
 const App = () => (
