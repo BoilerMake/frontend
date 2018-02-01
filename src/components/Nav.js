ae8fae8f82c4c2b35465e5f1fd9ef80669323f1a
CJ Enright <cenrigh@purdue.edu>
2018-01-31 19:45:28 -0500

    removed old code

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -9,7 +9,6 @@ class Nav extends Component {
       menu: false
     };
     this.toggleMenu = this.toggleMenu.bind(this);
-    this.handleLogout = this.handleLogout.bind(this);
     this.handleClick = this.handleClick.bind(this);
   }
   toggleMenu() {
@@ -17,10 +16,6 @@ class Nav extends Component {
   }
   handleClick(e) {
     this.setState({ menu: false });
-  }
-  handleLogout(e) {
-    this.setState({ menu: false });
-    this.props.logout();
   }
   render() {
     const { menu } = this.state;
@@ -67,70 +62,7 @@ class Nav extends Component {
         }
       </div>
     );
-      /*return (
-        <div className="navContainer">
-          <div className="navLogo">
-            <img src={logo} alt='boilermake' className='hammers'/>
-            <NavLink exact to='/' className='pink logo'>BoilerMake</NavLink>
-          </div>
-          <nav className="navBar desktop">
-            <NavLink exact to='/about' className='navLink hover'>About</NavLink>
-            <NavLink exact to='/contact' className='navLink hover'>Contact</NavLink>
-            { isLoggedIn ?
-              <NavLink exact to='/application' className='navLink hover' >application</NavLink>
-            :
-              null
-            }
-            {
-                isExec
-                ? <NavLink exact to='/exec' className='navLink hover'>exec dash</NavLink>
-                : null
-            }
-            { isLoggedIn ? (
-              <NavLink exact to="/" className="btn" onClick={this.handleLogout}>
-                Log Out
-              </NavLink>
-            ) : (
-              <NavLink exact to="/login" className="btn">
-                Login
-              </NavLink>
-            )}
-          </nav>
-          <nav className="navBar mobile">
-            <button onClick={this.toggleMenu}>
-              <span className="bar"></span>
-              <span className="bar"></span>
-              <span className="bar"></span>
-            </button>
-          </nav>
-          { menu ? (
-            <div className="menu" onClick={this.handleClick}>
-              <NavLink exact to='/about' className='navLink hover'>About</NavLink>
-              <NavLink exact to='/contact' className='navLink hover'>Contact</NavLink>
-              { isLoggedIn ? (
-                <NavLink exact to='/' onClick={this.handleLogout} className='navLink'><span className="hover">Log Out</span></NavLink>
-              ) : (
-                <NavLink exact to='/login' className='navLink'><span className="hover">Login</span></NavLink>
-              )}
-            </div>
-          ) : null
-          }
-        </div>
-      );*/
-    }
+  }
 }
 
-import { connect } from 'react-redux'
-import { logoutUser } from '../actions/users';
-function mapStateToProps (state) {
-    return {
-        isLoggedIn: state.user.authenticated,
-        isExec: state.user.isExec
-    };
-}
-
-const mapDispatchToProps = (dispatch, ownProps) => ({
-    logout: () => {dispatch(logoutUser())}
-});
-
-export default connect(mapStateToProps, mapDispatchToProps)(Nav);
+export default Nav;
