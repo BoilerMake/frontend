13e55d59cd6e07db7e323fba63f779a0d88d4f02
CJ Enright <cje@aaichicago.com>
2018-08-13 23:43:18 -0500

    Navbar updates (#104)
    * navbar has been refreshed
    
    * link updates

--- src/components/Nav/Nav.js
+++ src/components/Nav/Nav.js
@@ -8,10 +8,15 @@ import './_pillar.nav.source.scss';
 class Nav extends Component {
   constructor(props) {
     super(props);
-    this.state = { redirectToReferrer: false };
+    this.state = {
+      redirectToReferrer: false,
+      showing: false
+    };
   }
 
   render() {
+    const { showing } = this.state;
+
     return (
       <div className="p-nav">
         <div className="p-nav_content">
@@ -24,18 +29,77 @@ class Nav extends Component {
               />
             </Link>
           </div>
+          <nav className="p-nav__nav_links--mobile">
+            <span
+              className="p-nav__dropdown_mobile"
+              onClick={() => this.setState({ showing: !showing })}
+            >
+              Menu
+              {showing ? (
+                <div className="p-nav__dropdown_mobile--content">
+                  <div className="p-nav__dropdown--account">
+                    <NavLink
+                      exact
+                      to="/register"
+                      className="p-nav__dropdown_link--apply"
+                    >
+                      Apply
+                    </NavLink>
+                    <NavLink exact to="/login" className="p-nav__dropdown_link">
+                      Login
+                    </NavLink>
+                  </div>
+                  <hr />
+                  <NavLink exact to="/faq" className="p-nav__dropdown_link">
+                    FAQ
+                  </NavLink>
+                  <NavLink exact to="/hackers" className="p-nav__dropdown_link">
+                    Hackers
+                  </NavLink>
+                  <NavLink
+                    exact
+                    to="/sponsors"
+                    className="p-nav__dropdown_link"
+                  >
+                    Sponsors
+                  </NavLink>
+                  <NavLink exact to="/about" className="p-nav__dropdown_link">
+                    Team
+                  </NavLink>
+                </div>
+              ) : null}
+            </span>
+          </nav>
+
           <nav className="p-nav__nav_links">
-            <NavLink exact to="/hackers" className="p-nav__nav_link">
-              Hackers
-            </NavLink>
-            <NavLink exact to="/sponsors" className="p-nav__nav_link">
-              Sponsors
-            </NavLink>
-            <NavLink exact to="/about" className="p-nav__nav_link">
-              About
-            </NavLink>
+            <div className="p-nav__dropdown">
+              <NavLink exact to="/about" className="p-nav__nav_link">
+                About ▾
+              </NavLink>
+              <div className="p-nav__dropdown_content">
+                <NavLink exact to="/hackers" className="p-nav__dropdown_link">
+                  Hackers
+                </NavLink>
+                <NavLink exact to="/sponsors" className="p-nav__dropdown_link">
+                  Sponsors
+                </NavLink>
+                <NavLink exact to="/about" className="p-nav__dropdown_link">
+                  Team
+                </NavLink>
+              </div>
+            </div>
             <NavLink exact to="/faq" className="p-nav__nav_link">
               FAQ
+            </NavLink>
+            <NavLink exact to="/login" className="p-nav__nav_link">
+              Login
+            </NavLink>
+            <NavLink
+              exact
+              to="/register"
+              className="p-nav__nav_link p-nav__nav_link--apply"
+            >
+              Apply
             </NavLink>
             {/* Had this in for testing */}
             <a className="p-nav__nav_link" onClick={this.props.logoutUser}>
