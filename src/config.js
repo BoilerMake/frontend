6ef9a1de9c1fb8ec271247b834d3a925fd0ea720
Nicky Semenza <nicky@nickysemenza.com>
2018-10-11 10:05:43 +0100

    remove sentry in favor of rollbar (#156)

--- src/config.js
+++ src/config.js
@@ -1,6 +1,6 @@
-export const GOOGLE_ANALYTICS_ID = process.env.BM_REACT_APP_GOOGLE_ANALYTICS_ID || '';
+export const GOOGLE_ANALYTICS_ID =
+  process.env.BM_REACT_APP_GOOGLE_ANALYTICS_ID || '';
 export const DEBUG_MODE = process.env.BM_REACT_APP_DEBUG_MODE || false;
-export const SENTRY_URL = process.env.BM_REACT_APP_SENTRY_URL || '';
 export const API_BASE_URL = process.env.BM_REACT_APP_API_BASE_URL;
 export const GITHUB_CLIENT_ID = process.env.BM_REACT_APP_GITHUB_CLIENT_ID || '';
 export const ALLOW_SIGNUPS = process.env.BM_REACT_APP_ALLOW_SIGNUPS || false;
