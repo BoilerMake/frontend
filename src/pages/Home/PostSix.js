0e04a0e8444b74cd85397a139ae7c1b44e5f6129
CJ Enright <cje@aaichicago.com>
2018-11-29 18:23:12 -0500

    Adds application banner (#169)

--- src/pages/Home/PostSix.js
+++ src/pages/Home/PostSix.js
@@ -1,4 +1,5 @@
 import React, { PureComponent } from 'react';
+import { Button } from 'bm-kit';
 
 import BackCloud from '../../assets/images/hero/back_cloud_dark.svg';
 import MiddleCloud from '../../assets/images/hero/middle_cloud_dark.svg';
@@ -9,12 +10,20 @@ import Hammers from '../../assets/images/dark_hammers.svg';
 import './_pillar.post_six.source.scss';
 
 class PostSix extends PureComponent {
+  redirectToApplication() {
+    window.location =
+      'https://docs.google.com/forms/d/e/1FAIpQLSd5hvEUwVlCa69NwRkkzXJalt4FiRzDBbDMo5gl41v4Z2vyOw/viewform';
+  }
+
   render() {
     return (
       <div className="p-post_six">
+        <div className="p-post_six__recruitment paddingx">
+          <p className="paddingr">BoilerMake is looking for new members!</p>
+          <Button onClick={this.redirectToApplication}>Apply</Button>
+        </div>
         <div className="p-post_six__note">
           <img src={Hammers} className="p-post_six__hammers" alt="" />
-
           <p>
             Everything we do is for you -<br />
             the hackers, the designers,
@@ -31,9 +40,11 @@ class PostSix extends PureComponent {
           <p>Thank you for letting us inspire you.</p>
           <p className="signature">- The BoilerMake VI Team</p>
         </div>
-        <img src={BackCloud} className="p-post_six__back_cloud" alt="" />
-        <img src={MiddleCloud} className="p-post_six__middle_cloud" alt="" />
-        <img src={FrontCloud} className="p-post_six__front_cloud" alt="" />
+        <div className="p-post_six__clouds">
+          <img src={BackCloud} className="p-post_six__cloud" alt="" />
+          <img src={MiddleCloud} className="p-post_six__cloud" alt="" />
+          <img src={FrontCloud} className="p-post_six__cloud" alt="" />
+        </div>
       </div>
     );
   }
